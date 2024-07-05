fd302c8c922cc8c4eee5a5d19a356c2c9ae05ad3
zyalanaM <phyllisjeanstewart@gmail.com>
2024-07-05 11:45:52 -0400

    string

--- javascript-total-recall.js
+++ javascript-total-recall.js
@@ -23,5 +23,29 @@ console.log(z)
 // 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
 // you should spend over half the time planning (over 60%), the time should be spent in solving the actual coding should take less time.
 
+=====================Strings==================================
+  
+// 1. Create a variable called firstVariable?
+// let firstVariable;
 
+// 2. Assign it the value of the string "Hello World"?
+// let firstVariable = "Hello World";
+
+// 3. Change the value of this variable to some number?
+// let firstVariable = 3;
+
+// 4. Store the value of firstVariable in a new variable called secondVariable?
+// let secondVariable = 3;
+
+// 5. Change the value of secondVariable to any string?
+  // secondVariable = "Go Buckeyes";
+
+// 6. What is the value of firstVariable? 
+// console.log(firstvariable);
+  
+// 7. Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+// ex: Hello, my name is Jean Valjean
+
+// 8. let yourName = "Phyllis"
+// let greeting = "Hello, my name is " + yourName;
 
