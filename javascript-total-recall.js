3c51572cc1dfb6415ef5b4645d764fe6628aecc3
zyalanaM <phyllisjeanstewart@gmail.com>
2024-07-05 11:24:13 -0400

    Q & A

---
+++ javascript-total-recall.js
@@ -0,0 +1,27 @@
+//Q & A
+
+// 1. How do we assign a value to a variable? 
+//With the assignment operator.
+let z = 3;
+console.log(z)
+// 2. How do we change the value of a. 
+//By changing LET.
+
+//3. How do we assign an existing variable to a new variable?
+// the first and easiest way of changing a value of a variable is simply to reassign it:
+//example, let numberOfCats = 3; numberOfCats = 4;
+
+//4. Remind me, what are declare, assign, and define?
+// Declare- using, VAR (only in older), Let, const.
+// Assign- using the assignment operator.
+// Define- a variable is to give the variable a value.
+
+// 5. What is pseudocoding and why should you do it? 
+//A Pseudocode is defined as a step-by-step description of an algorithm. Pseudocode does not use any programming language in its representation instead it uses the simple English language text as it is intended for human understanding rather than machine reading.
+//Pseudocode is the intermediate state between an idea and its implementation(code) in a high-level language.
+
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// you should spend over half the time planning (over 60%), the time should be spent in solving the actual coding should take less time.
+
+
+
